b8a1eefd5007937e2db21221f2948c5cce948fe3
Nico-Posada <102486290+Nico-Posada@users.noreply.github.com>
2023-12-02 02:01:44 -0500

    Update part2.pl

--- Day-1/part2.pl
+++ Day-1/part2.pl
@@ -1,3 +1,2 @@
 #!/usr/bin/perl -p
-sub f{(($i)=grep$a[$_]=~@_[0],0..8)?$i+1:@_[0]}
-$r+=(f/(?=(\d|${\(join'|',@a=(one,two,three,four,five,six,seven,eight,nine))}))/g).f$1}{$_=$r
+sub f{(($i)=grep$a[$_]=~@_[0],0..8)?$i+1:@_[0]}$r+=(f/(?=(\d|${\(join'|',@a=(one,two,three,four,five,six,seven,eight,nine))}))/g).f$1}{$_=$r
