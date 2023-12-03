6bcc6bcfa5c971f7ff018e81f486ccb3f2bda339
Nico-Posada <102486290+Nico-Posada@users.noreply.github.com>
2023-12-03 14:41:09 -0500

    Update part1.rb

--- Day-3/part1.rb
+++ Day-3/part1.rb
@@ -1,6 +1,6 @@
 #!ruby
 t=0
 (h=*$<).zip(0..){|s,y|s.scan(/\d+/){|m|a,b=$~.offset w=0
-(a...b).map{|x|(r=*-1..1).product(r){nx,ny=x+_2,y+_1;0<=nx&&nx<140&&0<=ny&&ny<140&&h[ny][nx]!~/[\d.]/&&w=m.to_i}}
+(a...b).map{|x|(r=*-1..1).product(r){u,z=x+_2,y+_1;0<=u&&u<140&&0<=z&&z<140&&h[z][u]!~/[\d.]/&&w=m.to_i}}
 t+=w}}
 p t
