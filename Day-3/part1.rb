ef72e0ea622f128164420205b71dfc47e1bec81c
Nico-Posada <102486290+Nico-Posada@users.noreply.github.com>
2023-12-03 18:55:19 -0500

    Update and rename part1.rb to part2.rb

--- Day-3/part1.rb
+++
@@ -1,6 +0,0 @@
-#!ruby
-t=0
-(h=*$<).zip(0..){|s,y|s.scan(/\d+/){|m|a,b=$~.offset w=0
-(a...b).map{|x|(r=*-1..1).product(r){u,z=x+_2,y+_1;0<=u&&u<140&&0<=z&&z<140&&h[z][u]!~/[\d.]/&&w=m.to_i}}
-t+=w}}
-p t
