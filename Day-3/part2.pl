add2ff71a8ea34a30c23b6353ecc13b07a0871cc
Nico-Posada <102486290+Nico-Posada@users.noreply.github.com>
2023-12-03 22:48:51 -0500

    151 bytes day 3 part 2

--- Day-3/part2.pl
+++ Day-3/part2.pl
@@ -1,2 +1,2 @@
 #!perl -p
-push@s,$_;push@{$h{$.}},"$& @- @+"while/\d+/g}{s/\*/@a=map{grep{@p=split;$p[1]-2<"@-"<=$p[2]}@{$h{$y+$_}}}0..2;$t+=$a[0]*$a[1]if@a==2/eg,$y++for@s;$_=$t
+push@s,$_;push@{$h{$.}},"$& @- @+"while/\d+/g}{s/\*/@a=map{grep{@p=split;$p[1]-2<"@-"<=$p[2]}@{$h{$y+$_}}}0..2;$t+="@a"*$a[1]if@a==2/eg,$y++for@s;$_=$t
